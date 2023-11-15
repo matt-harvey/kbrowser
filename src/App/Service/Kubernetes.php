af1dee0addf6f0aaa980e5fc9039176a35b9da93
Matt Harvey <software@matthewharvey.net>
2023-11-15 22:59:12 +1100

    Code simplifications

--- src/App/Service/Kubernetes.php
+++ src/App/Service/Kubernetes.php
@@ -4,12 +4,19 @@ namespace App\Service;
 
 class Kubernetes
 {
+    /** @return array<string> the output */
+    private function runConsoleCommand(string $command): array
+    {
+        \exec($command, $output, $resultCode);
+        if ($resultCode != 0) {
+            throw new \Exception("Error executing command: $command");
+        }
+        return $output;
+    }
+
     private function doExpandPodName(string $namespace, string $podName): string
     {
-        \exec('kubectl get pods -o name', $pods, $resultCode);
-        if ($resultCode !== 0) {
-            throw new \Exception('Could not list pods to expand name');
-        }
+        $pods = $this->runConsoleCommand('kubectl get pods -o name');
         foreach ($pods as $pod) {
             if ($pod === $podName) {
                 return $pod;
@@ -21,62 +28,43 @@ class Kubernetes
     /** @return array<string> */
     public function getNamespaces(): array
     {
-        \exec('kubectl get namespaces -o name', $namespaces, $resultCode);
-        if ($resultCode != 0) {
-            throw new \Exception('Error getting namespaces');
-        }
+        $namespaces = $this->runConsoleCommand('kubectl get namespaces -o name');
         return \array_map(
             fn ($ns) => \preg_replace('/^namespace\//', '', $ns),
             $namespaces,
         );
     }
 
+    private function describeResource(string $resourceType, string $namespace, string $resourceName): string
+    {
+        $command = "kubectl describe $resourceType";
+        $command .= ' -n ' . \escapeshellarg($namespace);
+        $command .= ' ' . \escapeshellarg($resourceName);
+        $output = $this->runConsoleCommand($command);
+        return \implode(PHP_EOL, $output);
+
+    }
+
     public function describePod(string $pod, string $namespace): string
     {
-        $command = 'kubectl describe pod';
-        $command .= ' -n ' . \escapeshellarg($namespace);
-        $command .= ' ' . \escapeshellarg($pod);
-        \exec($command, $output, $resultCode);
-        if ($resultCode != 0) {
-            throw new \Exception('Could not describe pod');
-        }
-        return \implode(PHP_EOL, $output);
+        return $this->describeResource('pod', $namespace, $pod);
     }
 
     public function describeDeployment(string $deployment, string $namespace): string
     {
-        $command = 'kubectl describe deployment';
-        $command .= ' -n ' . \escapeshellarg($namespace);
-        $command .= ' ' . \escapeshellarg($deployment);
-        \exec($command, $output, $resultCode);
-        if ($resultCode != 0) {
-            throw new \Exception('Could not describe deployment');
-        }
-        return \implode(PHP_EOL, $output);
+        return $this->describeResource('deployment', $namespace, $deployment);
     }
 
     public function describeDaemonSet(string $daemonSet, string $namespace): string
     {
-        $command = 'kubectl describe daemonset';
-        $command .= ' -n ' . \escapeshellarg($namespace);
-        $command .= ' ' . \escapeshellarg($daemonSet);
-        \exec($command, $output, $resultCode);
-        if ($resultCode != 0) {
-            throw new \Exception('Could not describe daemonset');
-        }
-        return \implode(PHP_EOL, $output);
+        return $this->describeResource('daemonset', $namespace, $daemonSet);
     }
 
     public function getCurrentNamespace(): string
     {
-        \exec(
+        $currentNamespace = $this->runConsoleCommand(
             "kubectl ns --current view --minify -o jsonpath='{..namespace}'",
-            $currentNamespace,
-            $resultCode,
         );
-        if ($resultCode != 0) {
-            throw new \Exception('Error getting current namespace');
-        }
         if (\count($currentNamespace) != 1) {
             throw new \Exception('Error getting current namespace (non-zero count)');
         }
@@ -89,21 +77,16 @@ class Kubernetes
         $command = "kubectl get pods -o name";
         $escapedNamespace = \escapeshellarg($namespace);
         $command .= " --namespace=$escapedNamespace";
-        \exec($command, $pods, $resultCode);
-        if ($resultCode != 0) {
-            throw new \Exception('Error getting pods');
-        }
-        return \array_map(simplifiedPodName(...), $pods);
+        $output = $this->runConsoleCommand($command);
+        return \array_map(simplifiedPodName(...), $output);
     }
 
     /** @return array<array<string, string>> */
     public function getPodsWithNamespaces(): array
     {
-        $command = 'kubectl get pods -A -o custom-columns=":metadata.name,:metadata.namespace"';
-        \exec($command, $pods, $resultCode);
-        if ($resultCode != 0) {
-            throw new \Exception('Error getting pods');
-        }
+        $pods = $this->runConsoleCommand(
+            'kubectl get pods -A -o custom-columns=":metadata.name,:metadata.namespace"',
+        );
         $result = [];
         foreach ($pods as $line) {
             if (\strlen(\trim($line)) == 0) {
@@ -117,11 +100,7 @@ class Kubernetes
 
     public function getShortClusterName(): string
     {
-        $command = "kubectl config view -o jsonpath='{.clusters[].name}'";
-        \exec($command, $names, $resultCode);
-        if ($resultCode !== 0) {
-            throw new \Exception('Could not get cluster name');
-        }
+        $names = $this->runConsoleCommand("kubectl config view -o jsonpath='{.clusters[].name}'");
         if (\count($names) != 1) {
             throw new \Exception('Cannot determine single cluster from config');
         }
@@ -134,10 +113,7 @@ class Kubernetes
         $command = "kubectl get deployments -o name";
         $escapedNamespace = \escapeshellarg($namespace);
         $command .= " --namespace=$escapedNamespace";
-        \exec($command, $deployments, $resultCode);
-        if ($resultCode != 0) {
-            throw new \Exception('Error getting deployments');
-        }
+        $deployments = $this->runConsoleCommand($command);
         return \array_map(simplifiedDeploymentName(...), $deployments);
     }
 
@@ -145,10 +121,7 @@ class Kubernetes
     public function getDeploymentsWithNamespaces(): array
     {
         $command = 'kubectl get deployments -A -o custom-columns=":metadata.name,:metadata.namespace"';
-        \exec($command, $deployments, $resultCode);
-        if ($resultCode != 0) {
-            throw new \Exception('Error getting deployments');
-        }
+        $deployments = $this->runConsoleCommand($command);
         $result = [];
         foreach ($deployments as $line) {
             if (\strlen(\trim($line)) == 0) {
@@ -166,10 +139,7 @@ class Kubernetes
         $command = "kubectl get daemonsets -o name";
         $escapedNamespace = \escapeshellarg($namespace);
         $command .= " --namespace=$escapedNamespace";
-        \exec($command, $daemonSets, $resultCode);
-        if ($resultCode != 0) {
-            throw new \Exception('Error getting daemonsets');
-        }
+        $daemonSets = $this->runConsoleCommand($command);
         return \array_map(simplifiedDaemonSetName(...), $daemonSets);
     }
 
@@ -177,10 +147,7 @@ class Kubernetes
     public function getDaemonSetsWithNamespaces(): array
     {
         $command = 'kubectl get daemonsets -A -o custom-columns=":metadata.name,:metadata.namespace"';
-        \exec($command, $daemonSets, $resultCode);
-        if ($resultCode != 0) {
-            throw new \Exception('Error getting daemonsets');
-        }
+        $daemonSets = $this->runConsoleCommand($command);
         $result = [];
         foreach ($daemonSets as $line) {
             if (\strlen(\trim($line)) == 0) {
