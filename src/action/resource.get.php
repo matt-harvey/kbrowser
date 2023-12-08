40becc201e47f945a8dab30e248f9b05b24e1fe8
Matt Harvey <software@matthewharvey.net>
2023-12-09 08:15:21 +1100

    Link to controlling object from resource show page

--- src/action/resource.get.php
+++ src/action/resource.get.php
@@ -14,6 +14,25 @@ $objectKind = ObjectKind::from($objectKind);
 $objectName = $_GET['object'] or die('No object specified');
 $objectDescription = $kubernetes->describe($context, $objectKind, $namespace, $objectName);
 
+$lines = \explode(PHP_EOL, $objectDescription);
+$ownerUrl = null;
+$ownerName = null;
+foreach ($lines as $line) {
+    if (\preg_match('/^(Controlled By):\s+([^\s]+)$/', $line, $matches)) {
+        [$ownerKindStr, $ownerName] = \explode('/', $matches[2]);
+        $ownerKind = ObjectKind::tryFrom($ownerKindStr);
+        if ($ownerKind !== null) {
+            $ownerUrl = match (true) {
+                $ownerKind->isNamespaced() =>
+                    Route::forNamespacedResource($context, $ownerKind, $ownerName, $namespace)->toUrl(),
+                default =>
+                    Route::forNonNamespacedResource($context, $ownerKind, $ownerName)->toUrl(),
+            };
+            break;
+        }
+    }
+}
+
 $title = $objectName;
 $breadcrumbs = [
     Route::forHome()->toBreadcrumb(),
@@ -21,11 +40,15 @@ $breadcrumbs = [
     Route::forNamespaces($context)->toBreadcrumb(),
     Route::forNamespace($context, $namespace)->toBreadcrumb(),
     Route::forResources($context, $objectKind, $namespace)->toBreadcrumb(),
-    [\strval($objectName)  => null],
+    [\strval($objectName) => null],
 ];
 ?>
 
 <?php DefaultLayout::open($title, $breadcrumbs); ?>
+        <b><?= h("{$objectKind->value}/$objectName") ?></b>
+        <?php if ($ownerUrl !== null): ?>
+            [controlled by <a href="<?= $ownerUrl ?>"><?= h("$ownerKindStr/$ownerName") ?></a>]
+        <?php endif; ?>
     <div>
         <pre>
 <?= h($objectDescription) ?>
